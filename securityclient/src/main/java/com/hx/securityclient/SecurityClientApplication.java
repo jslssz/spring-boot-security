c67630bf50333aad4308eff156f9d52475809921
minghua.c <minghua.c>
2019-07-16 13:38:58 +0800

    update

--- securityclient/src/main/java/com/hx/securityclient/SecurityClientApplication.java
+++ securityclient/src/main/java/com/hx/securityclient/SecurityClientApplication.java
@@ -1,4 +1,4 @@
-package com.hx.securityserver;
+package com.hx.securityclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- securityclient/src/main/java/com/hx/securityclient/conf/SecurityConfiguration.java
+++ securityclient/src/main/java/com/hx/securityclient/conf/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.hx.securityserver.conf;
+package com.hx.securityclient.conf;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
